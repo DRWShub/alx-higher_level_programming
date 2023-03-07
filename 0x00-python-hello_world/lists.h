65b57b79386c8675e7a03600e6d093f72ec7ff9b
Eric Godworks <ericgodworks@yahoo.com>
2023-03-07 07:55:44 -0800

    list of prototypes

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -13,13 +13,13 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h); //prints all elements of a listint_t list
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
