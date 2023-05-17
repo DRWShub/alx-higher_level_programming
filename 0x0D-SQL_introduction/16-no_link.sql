9204578fb7e1ca150bd05c2a62fe63f8a11ffa85
Eric Godworks <ericgodworks@yahoo.com>
2023-05-17 06:04:31 -0700

    lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,6 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 -- list is sorted in descending order
-SELECT Â`score`, `name`
+SELECT `score`, `name`
 FROM `second_table`
-WHERE `name` != Â""
+WHERE `name` != ""
 ORDER BY `score` DESC;
