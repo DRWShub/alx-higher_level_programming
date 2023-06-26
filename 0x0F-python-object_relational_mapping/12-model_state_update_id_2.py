69a2290f09aad6161c10e9b6dc7d83b03b937927
Eric Godworks <ericgodworks@yahoo.com>
2023-06-26 12:19:21 -0700

    script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
-"""print State object with name passed as argument from
-   dtabase hbtn_0e_6_usa
-   """
-import sys
-from sqlalchemy import (create_engine)
-from model_state import Base, State
+'''script for task 12'''
+
+from model_state import State, Base
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
 
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    new_instance = session.query(State).filter_by(id=2).first()
-    new_instance.name = 'New Mexico'
-    session.commit()
+    local_session = Session()
+    state = local_session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    local_session.commit()
+
+    local_session.close()
+    engine.dispose()
