746095e72e1f894e9648fdf900380205d85d2003
DRWShub <ericgodworks@yahoo.com>
2023-07-02 21:32:28 -0700

    101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a JSON POST request to a given URL with a given JSON file.
-curl "$@" -sH "Content-Type: application/json" | grep -d "$(cat "$2")" "$1"
+curl "$@" -s | grep -H "Content-Type: application/json" | grep -d "$(cat "$2")" "$1"
