ccdac1503e25fd09637a714c5a7e55654d128fbf
DRWShub <ericgodworks@yahoo.com>
2023-07-06 10:41:54 -0700

    Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL and displays the value
-- of the X-Request-Id variable found in the header ofthe response.
-"""
-import sys
-import urllib.request
+"""The script sends POST request"""
+import requests
+from sys import argv
+
+
+def display_request(url: str, email_arg: str):
+    payload = {"email": email_arg}
+    req = requests.post(url, data=payload)
+    print(req.text)
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    display_request(argv[1], argv[2])
